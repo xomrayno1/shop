81fc206ef0cc085925d904e5b44f6c92fce3ae19
Tam Nguyen <tamnc@imt-soft.com>
2024-09-16 17:24:51 +0700

    add core processor

---
+++ CoreProcessor/src/main/java/com/shop/CoreProcessor/CoreProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.shop.CoreProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreProcessorApplication.class, args);
+	}
+
+}
---
+++ CoreProcessor/src/test/java/com/shop/CoreProcessor/CoreProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.CoreProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
